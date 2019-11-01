9168e125197cef3997789c98842bc64b9f6d1df6
sobhanm28 <sobhanm28@gmail.com>
2019-11-01 13:41:37 +0330

    --- Auto-Git Commit ---

--- lib/BasicData.dart
+++ lib/BasicData.dart
@@ -1,3 +1,3 @@
-    String basicUrl = "http://sarchak.com/sarchak/public";
-// String basicUrl = "http://192.168.43.15:8000";
+    // String basicUrl = "http://sarchak.com/sarchak/public";
+String basicUrl = "http://192.168.43.15:8000";
 
